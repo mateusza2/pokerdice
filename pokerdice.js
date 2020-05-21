08687562bc87d995362aa7fcf332eca3fb50ac6c
Mateusz Adamowski <mateusz@adamowski.pl>
2020-05-21 18:25:17 +0200

    buttony kości

--- pokerdice.js
+++ pokerdice.js
@@ -1,16 +1,31 @@
 
 function rzut_kostka(){
-    var poker = [ 'A', 'K', 'Q', 'J', 'T', '9' ]
+    var poker = [ 'A', 'K', 'Q', 'J', 'T', '9'   ]
     return poker[ Math.floor( Math.random() * 6 ) ]
 }
 
+function odswiez_kosci(){
+    for ( var p = 0; p < 2; p++ ){
+        for ( var d = 0; d < 5; d++ ){
+            var id = "#p" + p + "d" + d
+            document.querySelector( id ).textContent = Kosci_gracza[ p ][ d ]
+            document.querySelector( id ).className = "poker" + Kosci_gracza[ p ][ d ]
+        }
+    }
+
+
+}
 
 
 document.querySelector( "#rzut" ).addEventListener( "click", function(){
 
-    var wynik = rzut_kostka()
-
-    console.log( "Rzuca gracz nr " + Kolej_gracza + ": " + wynik )
+    for ( var i = 0; i < 5; i++ ){
+        console.log( Kosci_gracza[Kolej_gracza][i] )
+        if ( Kosci_gracza[Kolej_gracza][i] == '-' ){
+            Kosci_gracza[Kolej_gracza][i] = rzut_kostka()
+        }
+    }
+    odswiez_kosci()
 } )
 
 
